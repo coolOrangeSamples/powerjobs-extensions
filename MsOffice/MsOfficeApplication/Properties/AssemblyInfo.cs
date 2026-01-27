dc38a46a198a9adbd2c1d9eb30d5f28fb360f2f1
RossmeislThomas <thomas.rossmeisl@coolorange.com>
2026-01-27 18:01:49 +0100

    migrated MsOffice solution to .net8-windows

--- MsOffice/MsOfficeApplication/Properties/AssemblyInfo.cs
+++ MsOffice/MsOfficeApplication/Properties/AssemblyInfo.cs
@@ -1,19 +1,11 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
 
-[assembly: AssemblyTitle("MSOfficeApplication")]
-[assembly: AssemblyDescription("MSOfficeApplication for powerJobs Processor")]
-
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MSOfficeApplication")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("768B9BF9-254A-4158-8104-9A3D1844EE08")]
-
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: SupportedOSPlatform("windows")]
